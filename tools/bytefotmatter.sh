b23e3de6c418ce877c8044f559659ccd11e6bad8
Sang Hongwei <sanghongwei@megvii.com>
2018-06-19 19:06:21 +0800

    First add

---
+++ tools/bytefotmatter.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [[ $1 < 1024 ]]; then
+    echo "$1""B"
+elif [[ $1 -lt $((1024 ** 2)) ]]; then
+    fmt=$(printf "%.2f" `echo "scale=2;$1/1024"|bc`)
+    echo "$fmt""K"
+elif [[ $1 -lt $((1024 ** 3)) ]]; then
+    fmt=$(printf "%.2f" `echo "scale=2;$1/1024/1024"|bc`)
+    echo "$fmt""M"
+elif [[ $1 -lt $((1024 ** 4)) ]]; then
+    fmt=$(printf "%.2f" `echo "scale=2;$1/$((1024 ** 3))"|bc`)
+    echo "$fmt""G"
+elif [[ $1 -lt $((1024 ** 5)) ]]; then
+    fmt=$(printf "%.2f" `echo "scale=2;$1/$((1024 ** 4))"|bc`)
+    echo "$fmt""T"
+else
+    fmt=$(printf "%.2f" `echo "scale=2;$1/$((1024 ** 5))"|bc`)
+    echo "$fmt""P"
+fi
